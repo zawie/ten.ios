14b410cc3bde0d0c47b09f0dd69cf69341364101
Adam Zawierucha <zawiedev@gmail.com>
2025-12-25 12:31:54 -0800

    Embeds web view

--- ten/ContentView.swift
+++ ten/ContentView.swift
@@ -6,16 +6,26 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(url: URL(string: "https://10.zawie.io")!)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // No update needed for static URL
     }
 }
 
