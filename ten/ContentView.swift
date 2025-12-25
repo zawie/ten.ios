c3a4c17e726c78d8bc20efc1941e59168a716266
Adam Zawierucha <zawiedev@gmail.com>
2025-12-25 12:19:50 -0800

    Initial Commit

---
+++ ten/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ten
+//
+//  Created by Adam Zawierucha on 12/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ten/tenApp.swift
@@ -0,0 +1,17 @@
+//
+//  tenApp.swift
+//  ten
+//
+//  Created by Adam Zawierucha on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct tenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
